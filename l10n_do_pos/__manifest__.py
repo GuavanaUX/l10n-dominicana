094b9849075b49fb17ccff0e8f70474577a658f3
AlexDDLC <alexddlc139@gmail.com>
2025-08-26 23:46:09 -0400

    [UPD] upgrade xml to Odoo 18.0 removing attrs and include setting tag

--- l10n_do_pos/__manifest__.py
+++ l10n_do_pos/__manifest__.py
@@ -8,7 +8,7 @@
     'license': 'LGPL-3',
     'website': "https://github.com/odoo-dominicana",
     'category': 'Localization',
-    'version': '18.0.0.0.0',
+    'version': '18.0.0.0.2',
     'depends': [
         'base',
         'point_of_sale',
@@ -17,16 +17,16 @@
     'data': [
         'security/ir.model.access.csv',
         'data/data.xml',
-        'views/res_config_settings_views.xml',
+        #'views/res_config_settings_views.xml',
         'views/pos_order_views.xml',
         'views/pos_payment_method_views.xml',
     ],
-    'assets': {
-        'point_of_sale.assets': [
-            'l10n_do_pos/static/src/scss/*',
-            'l10n_do_pos/static/src/js/**/*.js',
-            'l10n_do_pos/static/src/xml/**/*.xml',
-        ],
-    },
+    # 'assets': {
+    #     'point_of_sale.assets': [
+    #         'l10n_do_pos/static/src/scss/*',
+    #         'l10n_do_pos/static/src/js/**/*.js',
+    #         'l10n_do_pos/static/src/xml/**/*.xml',
+    #     ],
+    # },
     'installable': True,
 }
