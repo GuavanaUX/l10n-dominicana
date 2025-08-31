3e89ea077b26519c6c189c567a2f93a337ca8299
AlexDDLC <alexddlc139@gmail.com>
2025-08-30 21:19:11 -0400

    [UPD] new fields and functions

--- dgii_reports/models/account_invoice.py
+++ dgii_reports/models/account_invoice.py
@@ -125,6 +125,8 @@ class AccountInvoice(models.Model):
                 #     tax_line_ids.filtered(
                 #         lambda tax: tax.tax_line_id.l10n_do_tax_type == 'prop').mapped('balance')
                 # ))
+
+                inv.proportionality_tax = inv.invoiced_itbis if inv.l10n_do_is_subject_to_proportionality else 0
                 inv.advance_itbis = inv.invoiced_itbis - inv.cost_itbis
 
     @api.depends(
@@ -159,7 +161,7 @@ class AccountInvoice(models.Model):
     @api.depends(
         'state',
         'invoice_date',
-        'invoice_line_ids',
+        'invoice_line_ids', 
         'invoice_line_ids.product_id',
         'invoice_line_ids.price_subtotal',
         'invoice_line_ids.balance'
@@ -423,6 +425,25 @@ class AccountInvoice(models.Model):
         "* The \'Grey\' status means Has not yet been reported or was partially reported.",
         default='normal'
     )
+    l10n_do_is_subject_to_proportionality = fields.Boolean( 
+        string='Subject to proportionality',
+        help='Indicates if the invoice is subject to proportionality tax.',
+        # default=lambda self: self._default_l10n_do_is_subject_to_proportionality()
+    )
+
+    @api.onchange('move_type')
+    def _default_l10n_do_is_subject_to_proportionality(self):
+        """Determines the default value for the field based on the company and move type."""
+        for record in self:
+            if record.company_id.l10n_do_is_subject_to_proportionality and self.move_type in ('in_invoice', 'out_invoice'):
+                record.l10n_do_is_subject_to_proportionality = True
+
+    @api.constrains('l10n_do_is_subject_to_proportionality')
+    def l10n_do_is_subject_to_proportionality_constrains(self):
+        for inv in self:
+            if inv.fiscal_status == 'done':
+                raise ValidationError(
+                    _('You cannot change the proportionality status of a reported invoice.'))
 
     @api.model
     def norma_recompute(self):
