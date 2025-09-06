a0135f846dee215873ce02ad6d1cfdcb53295002
AlexDDLC <alexddlc139@gmail.com>
2025-09-06 14:11:09 -0400

    [UPD] Removed country tag

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -13,7 +13,6 @@
     'website': 'https://github.com/odoo-dominicana',
     'category': 'Localization',
     'version': '18.0.0.1.2',
-    'countries': ['do'],
     # any module necessary for this one to work correctly
     'depends': [
         'base',
