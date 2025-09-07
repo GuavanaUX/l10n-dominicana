d26643ef07dfa7c2c164153c8fba889b22bfc8fa
AlexDDLC <alexddlc139@gmail.com>
2025-09-07 10:24:46 -0400

    [REN] Rename file

--- l10n_do_pos/static/src/js/payment_screen_extend.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module **/
-import { patch } from "@web/core/utils/patch";
-import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
-
-patch(PaymentScreen.prototype, {
-    async onClickTest() {
-        console.log("Clicked Test Button");
-    },
-});
