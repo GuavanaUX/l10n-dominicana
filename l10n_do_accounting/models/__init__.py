0a687159afe2404ef36754a48bc552943e252a6b
AlexDDLC <alexddlc139@gmail.com>
2025-11-10 17:04:03 -0400

    [DEL] Remove model to implement on another model

--- l10n_do_accounting/models/__init__.py
+++ l10n_do_accounting/models/__init__.py
@@ -3,6 +3,5 @@ from . import account_invoice
 from . import account_journal
 from . import account_invoice_cancel
 from . import account_payment_term
-from . import account_tax
 from . import res_partner
 # from . import account_invoice_send
